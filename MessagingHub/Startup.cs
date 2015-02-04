7732dbc599b357cf6a551124b911b1c7548703c4
tlkiong <kiong_90@yahoo.com>
2015-02-05 00:12:57 +0800

    Added basic SignalR dependencies from Nuget Packages

---
+++ MessagingHub/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MessagingHub.Startup))]
+namespace MessagingHub
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
